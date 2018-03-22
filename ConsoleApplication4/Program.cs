9d3545166562d0141300ccfc98e03fe874d181df
Ori Hirshfeld <ori81510@gmail.com>
2018-03-22 11:56:55 +0200

    mess

---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void initialize(out int x)
+        {
+            x = 0;
+        }
+
+        static void increment(ref int x)
+        {
+            x++;
+        }
+
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            do
+            {
+                Console.Write("Enter first number: ");
+            } while (!int.TryParse(Console.ReadLine(), out num1));
+            //kjiljklj
+            do
+            {
+                Console.Write("Enter second number: ");
+
+            } while (!int.TryParse(Console.ReadLine(), out num2));
+
+            Console.WriteLine($"sum is: {num1 + num2}");
+        }
+    }
+}
