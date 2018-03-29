53e2f312bfb0481e8a79b59d4bec27513b82e230
Ori Hirshfeld <ori81510@gmail.com>
2018-03-29 20:01:51 +0300

    implemented AddFile in ImageServiceModal.cs

--- ConsoleApplication4/ImageService/ImageService/Modal/ImageServiceModal.cs
+++ ConsoleApplication4/ImageService/ImageService/Modal/ImageServiceModal.cs
@@ -18,9 +18,64 @@ namespace ImageService.Modal
         private string m_OutputFolder;            // The Output Folder
         private int m_thumbnailSize;              // The Size Of The Thumbnail Size
 
+        public ImageServiceModal(string outputFolder)
+        {
+            m_OutputFolder = outputFolder;
+        }
+
         public string AddFile(string path, out bool result)
         {
-            throw new NotImplementedException();
+            if (File.Exists(path))
+            {
+
+                try
+                {
+                    if (!Directory.Exists(m_OutputFolder))
+                    {
+                        Console.WriteLine("m_outputFolder didn't exist");
+                        Directory.CreateDirectory(m_OutputFolder);
+                    }
+
+                    var x = File.GetCreationTime(path);
+
+                    int year = x.Year;
+                    int month = x.Month;
+                    var yearPath = m_OutputFolder + "\\" + year;
+                    var monthPath = yearPath + "\\" + month;
+                    var filePath = monthPath + "\\" + Path.GetFileName(path);
+                    if (!Directory.Exists(yearPath))
+                    {
+                        Console.WriteLine("yearPath directory didn't exist");
+                        Directory.CreateDirectory(yearPath);
+                    }
+
+                    if (!Directory.Exists(monthPath))
+                    {
+                        Console.WriteLine("monthPath directory didn't exist");
+                        Directory.CreateDirectory(monthPath);
+                    }
+
+                    if (!File.Exists(filePath))
+                    {
+                        File.Copy(path, filePath);
+                    }
+
+                    result = true;
+                    return null;
+                }
+                catch (Exception ex)
+                {
+                    result = false;
+                    Console.WriteLine("cought an exception");
+                    Console.WriteLine(ex.Message);
+                    return ex.Message;
+                }
+            }
+
+            result = false;
+            return null;
+
+            //(?) create thumbnail
         }
 
         #endregion
