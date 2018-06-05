63a40c4af20b488cde7fa71bddf8ef2bb893b93f
mm <meirtor1@gmail.com>
2018-06-05 10:58:27 +0300

    Removed examples and arranged config

--- WebApplication2/WebApplication2/App_Start/RouteConfig.cs
+++ WebApplication2/WebApplication2/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebApplication2
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "First", action = "AjaxView", id = UrlParameter.Optional }
+                defaults: new { controller = "First", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
